e73eb07d1ac85f0774de7879f745e245dce191b7
asi-isa <ali.inceoglu@protonmail.com>
2022-12-20 09:04:04 +0100

    .

--- src/components/kanban/AddColumn.vue
+++ src/components/kanban/AddColumn.vue
@@ -3,12 +3,15 @@ import { IconPlus } from "@iconify-prerendered/vue-mdi";
 </script>
 
 <template>
-  <div
-    class="bg-[var(--background-secondary)] dark:bg-[var(--background-secondary-dark)] w-64 flex items-center justify-center rounded-md cursor-pointer"
-  >
-    <div class="flex gap-2 items-center">
-      <IconPlus class="text-xl" />
-      <p class="text-xl">New Column</p>
+  <div class="flex flex-col gap-4">
+    <div><p class="opacity-0">just for ui purposes</p></div>
+    <div
+      class="bg-[var(--background-secondary)] dark:bg-[var(--background-secondary-dark)] w-64 h-full flex items-center justify-center rounded-md cursor-pointer"
+    >
+      <div class="flex gap-2 items-center">
+        <IconPlus class="text-xl" />
+        <p class="text-xl">New Column</p>
+      </div>
     </div>
   </div>
 </template>
